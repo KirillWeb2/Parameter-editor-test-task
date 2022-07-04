925e8b8737d2013e85f388043a19e63c10118222
kirill <kirill2004pro1000@mail.ru>
2022-07-04 16:53:16 +0300

    first

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,192 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from "react"
+import EditIcon from '@mui/icons-material/Edit'
+import DeleteIcon from '@mui/icons-material/Delete'
+import { Box, Button, TextField, Typography } from "@mui/material"
+
+interface Param {
+  id: number
+  name: string
+  type: string
+}
+interface ParamValue {
+  paramId: number
+  value: string
+}
+interface Model {
+  paramValues: ParamValue[]
+  colors?: String[]
+}
+interface State {
+  params: Param[]
+  values: ParamValue[]
+  isNew: boolean
+  name: string
+  value: string
+  changeItemId: number
+}
+interface Props {
+  params: Param[]
+  model: Model
 }
 
-export default App;
+
+class ParamEditor extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props)
+    this.state = {
+      params: this.props.params,
+      values: this.props.model.paramValues,
+      isNew: false,
+      name: "",
+      value: "",
+      changeItemId: 0
+    }
+
+    this.changeParams = this.changeParams.bind(this)
+    this.openForm = this.openForm.bind(this)
+    this.changeForm = this.changeForm.bind(this)
+    this.changeValue = this.changeValue.bind(this)
+    this.deleteParams = this.deleteParams.bind(this)
+  }
+
+  openForm(): void {
+    this.setState({ isNew: true })
+  }
+
+  changeValue(parameter: Param, value: ParamValue): void {
+    this.setState({
+      isNew: true,
+
+      value: value.value,
+      name: parameter.name,
+
+      changeItemId: parameter.id
+    })
+  }
+
+  changeForm(e: React.ChangeEvent<HTMLInputElement>): void {
+    switch (e.target.name) {
+      case "name": this.setState({ name: e.currentTarget.value })
+        break
+      case "value": this.setState({ value: e.currentTarget.value })
+        break
+      default: return
+    }
+  }
+
+  deleteParams(id: number): void {
+    this.setState({
+      params: this.state.params.filter(i => i.id !== id),
+      values: this.state.values.filter(i => i.paramId !== id)
+    })
+  }
+
+  changeParams(): void {
+    if (!this.state.name || !this.state.value) {
+      return this.setState({ isNew: false, value: '', name: '' })
+    }
+
+    const newParams: Param = {
+      id: this.state.params.length + 1,
+      name: this.state.name,
+      type: "string"
+    }
+
+    const newValue: ParamValue = {
+      paramId: newParams.id,
+      value: this.state.value
+    }
+
+    if (this.state.changeItemId) {
+      // this.state.changeItemId параметр позволяющий определить, какой параметр изменить
+      // если этот параметр есть, значит обновляем, если нет - добавляем параметр
+
+      this.setState({
+        params: this.state.params.map(i => i.id === this.state.changeItemId
+          ? { ...i, name: this.state.name }
+          : i
+        ),
+        values: this.state.values.map(i => i.paramId === this.state.changeItemId
+          ? { ...i, value: this.state.value }
+          : i
+        ),
+
+        value: "",
+        name: "",
+
+        isNew: false,
+        changeItemId: 0
+      })
+    } else {
+      this.setState({
+        params: [...this.state.params, newParams],
+        values: [...this.state.values, newValue],
+
+        value: "",
+        name: "",
+
+        isNew: false,
+        changeItemId: 0
+      })
+    }
+  }
+
+
+  render(): React.ReactNode {
+    return (
+      <Box className="app">
+        <Typography variant="h1" component="div">
+          Parameter editor
+        </Typography>
+        <Box className="params">
+          {this.state.params.map(i =>
+            <Box key={i.id} className="params__item">
+              <Typography className="params__item_left" variant="body1" component="div">
+                {i.name}
+              </Typography>
+              <TextField
+                id="standard-basic"
+                className="params__item_middle"
+                label={i.name}
+                variant="standard"
+                value={this.state.values[i.id - 1].value}
+              />
+              <Box className="params__item_right">
+                <EditIcon className="change" onClick={() => this.changeValue(i, this.state.values[i.id - 1])} />
+                <DeleteIcon className="delete" onClick={() => this.deleteParams(i.id)} />
+              </Box>
+            </Box>
+          )}
+          {this.state.isNew &&
+            <Box className="item__change">
+              <TextField
+                id="standard-basic"
+                label='Название параметра'
+                className="item__change_left"
+                name="name"
+                variant="standard"
+                value={this.state.name}
+                onChange={this.changeForm}
+              />
+              <TextField
+                id="standard-basic"
+                label="Значение"
+                className="item__change_right"
+                name="value"
+                variant="standard"
+                value={this.state.value}
+                onChange={this.changeForm}
+              />
+            </Box>
+          }
+          {this.state.isNew && <Button onClick={this.changeParams} variant="outlined">Готово</Button>}
+          {!this.state.isNew && <Button onClick={this.openForm} variant="outlined">Добавить параметр</Button>}
+        </Box>
+
+      </Box>
+    )
+  }
+}
+
+export default ParamEditor
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import ParamEditor from './App'
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import './index.css'
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+
+const params = [
+  {
+    id: 1,
+    name: "Назначение",
+    type: "string"
+  },
+  {
+    id: 2,
+    name: "Длина",
+    type: "string"
+  },
+  {
+    id: 3,
+    name: "Сфера применения",
+    type: "string"
+  }
+]
+
+
+const model = {
+  paramValues: [
+    {
+      paramId: 1,
+      value: "повседневное"
+    },
+    {
+      paramId: 2,
+      value: "максимальная"
+    },
+    {
+      paramId: 3,
+      value: "IT"
+    }
+  ]
+}
+
+
+root.render(<ParamEditor model={model} params={params} />)
